a2902d3a1f4c9bc6d99edf2c5b89f3a1c3f62560
Yanis Falaki <yanisfalaki@outlook.com>
2022-08-04 18:23:45 -0400

    Finished thermostat

--- src/App.js
+++ src/App.js
@@ -2,17 +2,35 @@ import {useState} from "react"
 import './App.css';
 
 function App() {
-  const [temp, setTemp] = useState(0)
+  const [temp, setTemp] = useState(22)
+  const [isHot, setIsHot] = useState(true)
+
+  const changeTemp = (type) => {
+    if (type === "raise"){
+      setTemp(temp + 1)
+      if (temp + 1 >= 22) { setIsHot(true) }
+    }
+    else if (type === "lower") {
+      setTemp(temp - 1)
+      if (temp - 1 < 22) { setIsHot(false) }
+    }
+    else {
+      console.error("changeTemp() invalid or missing type")
+    }
+  }
 
   return (
     <div className="App">
       <div className="container">
-        <div className="circle">
-          <h1>{temp}</h1>
+        <div className="circle" 
+          style= {{
+              backgroundColor: isHot ? 'crimson' : 'blue'
+            }}>
+          <h1>{temp}°</h1>
         </div>
         <div className="button-wrapper">
-          <button className="button-red">Raise Temperature</button>
-          <button className="button-blue">Lower Temperature</button>
+          <button className="button-red" onClick={() => changeTemp("raise")}>Raise Temperature</button>
+          <button className="button-blue" onClick={() => changeTemp("lower")}>Lower Temperature</button>
         </div>
       </div>
 
