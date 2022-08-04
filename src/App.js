3c2daaa0780dade8d2cf0292492b4f2b5c57f229
Yanis Falaki <yanisfalaki@outlook.com>
2022-08-04 03:03:43 -0400

    Added UI Elements

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
+import {useState} from "react"
 import './App.css';
 
 function App() {
+  const [temp, setTemp] = useState(0)
+
   return (
     <div className="App">
-      
+      <div className="container">
+        <div className="circle">
+          <h1>{temp}</h1>
+        </div>
+        <div className="button-wrapper">
+          <button className="button-red">Raise Temperature</button>
+          <button className="button-blue">Lower Temperature</button>
+        </div>
+      </div>
+
     </div>
   );
 }
